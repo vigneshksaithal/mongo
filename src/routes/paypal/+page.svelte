45d583c246b535d285a85e971f5182bd562bd54b
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-01-03 10:32:11 +0530

    Add PayPal payment form component
    - Introduced a new Svelte component for PayPal payment integration.
    - The form includes a "Pay Now" button styled with custom CSS and displays PayPal branding.
    - The form action points to the PayPal payment URL for processing transactions.

---
+++ src/routes/paypal/+page.svelte
@@ -0,0 +1,37 @@
+<form
+	action="https://www.paypal.com/ncp/payment/PN4JGD99V8XDU"
+	method="post"
+	target="_top"
+	style="display:inline-grid;justify-items:center;align-content:start;gap:0.5rem;"
+>
+	<input class="pp-PN4JGD99V8XDU" type="submit" value="Pay Now" />
+	<img
+		src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg"
+		alt="cards"
+	/>
+	<section>
+		Powered by <img
+			src="https://www.paypalobjects.com/paypal-ui/logos/svg/paypal-wordmark-color.svg"
+			alt="paypal"
+			style="height:0.875rem;vertical-align:middle;"
+		/>
+	</section>
+</form>
+
+<style>
+	.pp-PN4JGD99V8XDU {
+		text-align: center;
+		border: none;
+		border-radius: 0.25rem;
+		min-width: 11.625rem;
+		padding: 0 2rem;
+		height: 3.125rem;
+		font-weight: bold;
+		background-color: #ffd140;
+		color: #000000;
+		font-family: 'Helvetica Neue', Arial, sans-serif;
+		font-size: 1.125rem;
+		line-height: 1.5rem;
+		cursor: pointer;
+	}
+</style>
